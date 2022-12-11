be0c8227c60a1cc346c556983beec747843182ff
mikigroup <mikigroup@gmail.com>
2022-12-11 13:38:31 +0100

    GA script

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,6 +1,6 @@
-<header class="p-3 md:p-6 border border-slate-300 rounded-lg animate__animated animate__fadeIn animate__slow">
+<header class="grid justify-items-center p-3 md:p-3 border border-slate-300 rounded-lg animate__animated animate__fadeIn animate__slow">
   <nav>
-    <div class="border-2 rounded-lg text-center grid bg-slate-100 shadow-lg">      
+    <div class="p-2 flex w-full grid items-center place-content-center justify-center justify-items-center content-center border-2 rounded-lg text-center grid bg-slate-100 shadow-lg">      
       <div class="grid content-center p-4">
         <p class="grid text-2xl md:text-3xl animate__animated animate__fadeIn animate__delay-2s animate__slower">
          Jiří Mika - Web developer
