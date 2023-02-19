9a6400f76a5490ac6ab906e8788c6da2dc200772
mikigroup <mikigroup@gmail.com>
2023-02-19 11:47:17 +0100

    1

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,14 +3,11 @@
   import Header from "$lib/components/Header.svelte";
   import Footer from "$lib/components/Footer.svelte";
 </script>
-
-<div class="">  <!-- bg-cover bg-center" style="background-image: url(/static/hor.svg) -->
-
 <Header />
-<div class="flex justify-center pt-5">
+
 <slot />
-</div>
-</div>
+
 <Footer />
+
 <style>
 </style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,5 @@
-<div class="bg w-full grid p-2 sm:pl-0 justify-center shadow-2xl m-3">
+<div class="grid col-1 justify-items-center">
+<div class="bg grid p-2 m-3 pt-5 sm:pl-0 shadow-2xl justify-center">
   <div class="grid grid-rows-4">
     <div class="grid justify-center mt-5">
       <img class="opacity-90 spock w-24 md:w-32" src="hand-spock.svg" alt="miki spock" />
@@ -7,6 +8,18 @@
     <div class="image2 Lighters text-5xl text-white animate__animated animate__fadeInRight">Group</div>
   </div>  
 </div>
+</div>
+<div class="grid grid-cols-3 pt-10 w-full">  
+  <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original-wordmark.svg" width="400"/>                
+  <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original-wordmark.svg" width="400"/>  
+  <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" width="400"/>                    
+</div>
+
+
+
+
+
+
 
 <style>
   .heavy {
