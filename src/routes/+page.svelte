a2ae30ed9992be8dc851b79d44d8930b63705948
mikigroup <39709130+mikigroup@users.noreply.github.com>
2023-08-02 16:18:27 +0200

    1

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,7 +3,7 @@
     <div class="grid grid-rows-4">
       <div class="grid justify-center mt-5">
         <img
-          class="opacity-90 spock w-24 md:w-32 animate__animated animate__fadeIn animate__delay-2s "
+          class="opacity-90 spock w-24 md:w-32 animate__animated animate__fadeIn animate__delay-2s"
           src="hand-spock.svg"
           alt="miki spock"
         />
@@ -27,7 +27,9 @@
 <div class="grid grid-cols-3">
   <div />
   <div class="grid">
-    <h2 class="text-2xl mb-5 p-5 border-2 text-center">70% kodér - 30% programátor</h2>
+    <h2 class="text-2xl mb-5 p-5 border-2 text-center">
+      70% kodér - 30% programátor
+    </h2>
     <h3 class="text-xl">// v čem rád dělám</h3>
     <div class="">
       <li>Tailwind</li>
@@ -41,6 +43,29 @@
     </div>
   </div>
   <div />
+</div>
+
+<div class="mt-10">
+  Využijte sílu digitálních řešení na míru se zkušeným programátorem na volné
+  noze. Jsem vybaven silnými základy v jazycích HTML, CSS a JavaScript a
+  pokročilou znalostí jazyka Svelte, a proto dodávám responzivní a uživatelsky
+  přívětivé webové aplikace šité na míru vašim jedinečným obchodním cílům. Moje
+  cesta v programování trvá několik let, během nichž jsem si vypěstoval bohaté
+  znalosti o složitosti vývoje webových aplikací. Jsem zběhlý ve vytváření
+  škálovatelných a na výkon orientovaných frontendových architektur s využitím
+  nejnovějších webových technologií, přičemž se zaměřuji na psaní čistého,
+  efektivního a udržovatelného kódu. V rozsáhlé oblasti JavaScriptu sahají mé
+  odborné znalosti až k inovativnímu, na komponentách založenému frameworku
+  Svelte. Svelte, který je uznávaný pro vytváření vysoce výkonného kódu díky
+  přesunu zátěže z běhu na krok kompilace, otevírá bránu k vysoce
+  optimalizovanému prostředí pro koncové uživatele. Moje služby nejsou jen o
+  kódování, ale také o vytváření digitálních zážitků. Moje zběhlost v jazycích
+  HTML, CSS a JavaScript mi ve spojení se softwarem Svelte umožňuje vytvářet
+  vizuálně atraktivní, rychlé a interaktivní webové stránky a webové aplikace.
+  Můj závazek dodržovat osvědčené postupy při kódování navíc zajišťuje, že vaše
+  projekty budou nejen funkční, ale také vhodné pro SEO a přístupné. Jste
+  připraveni proměnit své nápady v digitální realitu? Pojďme společně vytvořit
+  něco pozoruhodného.
 </div>
 
 <div class="grid grid-cols-3 justify-items-center my-10 md:p-20 border-2">
