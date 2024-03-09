8965bc7e85ec0ef30e7c47f6d5327da9585738e5
mikigroup <mikigroup@gmail.com>
2024-03-09 22:34:58 +0100

    1

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,6 +1,7 @@
 <footer class="md:m-6 border border-slate-300 rounded-lg shadow-md grid grid-cols-2">
   <div class="grid content-center ml-5">
-    <p class="text-md"><i class="fa fa-regular fa-hand-spock mr-2 text-gray-500"></i>Email: mikigroup@gmail.com
+    <p class="text-md">mikigroup@gmail.com <br>
+    +420 777 777 777 <br>
       </p>
   </div>
   <div class="">    
@@ -8,7 +9,7 @@
           <p class="grid place-content-end text-sm m-5">
             Zapsán u Živnostenského úřadu v Jeseníku <br>
             IČO: 01645803 - nejsem plátce DPH<br>
-            2023 - Všechny práva vyhrazena-rev20102023          
+            2020-2024  Všechny práva vyhrazena _rev2024          
           </p>        
       </div>    
   </div>
