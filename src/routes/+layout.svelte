e64203eeebe391e56dc98b546a65392aa0f3101c
mikigroup <mikigroup@gmail.com>
2024-03-09 23:49:11 +0100

    Revert "1"
    This reverts commit 62e9021a26655e3915200246adde235dffe374e8.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,7 +2,6 @@
   import "../app.css";
   import Header from "$lib/components/Header.svelte";
   import Footer from "$lib/components/Footer.svelte";
-  import 'animate.css';
 </script>
 <Header />
 
