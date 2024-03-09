62e9021a26655e3915200246adde235dffe374e8
mikigroup <mikigroup@gmail.com>
2024-03-09 23:35:19 +0100

    1

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,7 @@
   import "../app.css";
   import Header from "$lib/components/Header.svelte";
   import Footer from "$lib/components/Footer.svelte";
+  import 'animate.css';
 </script>
 <Header />
 
